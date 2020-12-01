a39c760e6c4d95d267c8bb7730a58765de26e1e4
Rajput-Suraj <surajarajput@gmail.com>
2020-12-01 16:14:23 +0530

    Created and styled navbar of the application.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import Products from "./components/Products/Products";
+
+import { Navbar, Products } from "./components";
 
 function App() {
     return (
         <div>
+            <Navbar />
             <Products />
         </div>
     );
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+import {
+    AppBar,
+    Badge,
+    IconButton,
+    Menu,
+    MenuItem,
+    Toolbar,
+    Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import useStyles from "./styles";
+
+import Logo from "../../assets/commerce.png";
+
+const Navbar = () => {
+    const classes = useStyles();
+
+    return (
+        <>
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+                <Toolbar>
+                    <Typography
+                        variant="h6"
+                        className={classes.title}
+                        color="inherit"
+                    >
+                        <img
+                            src={Logo}
+                            alt="E-Commerce"
+                            height="25px"
+                            className={classes.image}
+                        />
+                        E-Commerce.js
+                    </Typography>
+                    <div className={classes.grow} />
+                    <div className={classes.button}>
+                        <IconButton
+                            aria-label="Show cart items"
+                            color="inherit"
+                        >
+                            <Badge badgeContent={2} color="secondary">
+                                <ShoppingCart />
+                            </Badge>
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+        </>
+    );
+};
+
+export default Navbar;
